c1d85505f0a67ee9193fce0c53c8bd969bc10076
guillermo <h.uhm292@gmail.com>
2023-04-14 09:38:22 +0200

    first commit

---
+++ src/test/java/com/guille/porti/PortiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guille.porti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
